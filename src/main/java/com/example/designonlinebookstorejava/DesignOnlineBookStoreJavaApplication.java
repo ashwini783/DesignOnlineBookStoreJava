4cbe3f410a2868324ea8d0b8e4b6cb5e09b728cd
Ashwini <ashwiniavhad8@gmail.com>
2025-03-22 21:07:59 +0530

    first commit

---
+++ src/main/java/com/example/designonlinebookstorejava/DesignOnlineBookStoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.designonlinebookstorejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignOnlineBookStoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignOnlineBookStoreJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/designonlinebookstorejava/DesignOnlineBookStoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designonlinebookstorejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignOnlineBookStoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
